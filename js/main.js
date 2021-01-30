f131298e3584a7d50e0f1a3542b0985c995c1121
Fahim Faysal Kabir <githubbadhon@gmail.com>
2021-01-30 20:00:52 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  // sticky menu
+  $(".js--sticky-menu").waypoint(function(direction){
+    if(direction=="down"){
+        $(".nav").addClass("sticky");
+    }
+    else{
+        $(".nav").removeClass("sticky");
+    }
+  });
+});
+// mobile menu
+function openNav(){
+  document.getElementById("myNav").style.width="100%";
+}
+function closeNav(){
+  document.getElementById("myNav").style.width="0%";
+}
