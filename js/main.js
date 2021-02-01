fd21b4620a6ba48fbb1432da5e5fd78fd06dbeaa
Fahim Faysal Kabir <githubbadhon@gmail.com>
2021-02-01 23:58:30 +0600

    1.2.2021

--- js/main.js
+++ js/main.js
@@ -16,3 +16,21 @@ function openNav(){
 function closeNav(){
   document.getElementById("myNav").style.width="0%";
 }
+$('.owl-carousel').owlCarousel({
+    stagePadding: 50,
+    loop:true,
+    margin:10,
+	  autoplay:true,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
